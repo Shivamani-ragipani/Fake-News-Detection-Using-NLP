54d4f5bf1eacf5117cac17ff74ce8ecbf80edc1f
Shivamani-ragipani <shivamaniragipani@gmail.com>
2025-04-06 18:07:32 +0530

    .

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ model = pickle.load(open("finalized_model.pkl", 'rb'))
 
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render assigns a dynamic port
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+
 @app.route('/')
 def home():
     return render_template("index.html")
