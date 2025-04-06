536b2f17908d4388d4be77f46daa3edf098a83a2
Shivamani-ragipani <shivamaniragipani@gmail.com>
2025-04-06 18:14:28 +0530

    .

--- app.py
+++ app.py
@@ -33,5 +33,7 @@ def prediction():
         return render_template("prediction.html", prediction_text="Enter a news headline.", gif_path=None)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Use Render-assigned port
+    app.run(debug=True, host='0.0.0.0', port=port)  # Bind to 0.0.0.0
+
